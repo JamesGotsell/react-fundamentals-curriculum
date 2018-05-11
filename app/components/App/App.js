60729d7f9a1410da337bd5b67c0351bdd72154cb
James <j.e.gotsell@gmail.com>
2018-05-11 19:07:01 +0100

    created new components

---
+++ app/components/App/App.js
@@ -0,0 +1,26 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
+var ZipCode = require('../ZipCode/ZipCode');
+
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className='container'>
+        <div>
+          <h1>Main Header</h1>
+        </div>
+        <div className='home-container' style={{ backgroundImage: "url('app/images/pattern.svg')" }}>
+          <h1 className='header'>Enter a City and State</h1>
+          <ZipCode
+            direction='column'
+            onSubmitZipcode={function () { }}
+            onUpdateZipcode={function () { }}
+            zipcode={123} />
+        </div>
+      </div>
+    )
+  }
+}
+
+module.exports = App;
---
+++ app/components/header/Header.js
@@ -0,0 +1,13 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
+
+
+class Header extends React.Component {
+  render() {
+    return (
+      <div>Header</div>
+    )
+  }
+}
+
+module.exports = Header
